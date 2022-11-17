eb1b0542ef02c7d39a50bdd066ee8a2b04cf2c83
Mlie <emipa606@users.noreply.github.com>
2022-11-17 21:10:50 +0100

    First publish

---
+++ Source/Adjacent Light/HarmonyPatches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Adjacent_Light;
+
+[StaticConstructorOnStartup]
+internal static class HarmonyPatches
+{
+    static HarmonyPatches()
+    {
+        var harmony = new Harmony("AdjacentLight");
+        harmony.PatchAll();
+    }
+}
---
+++ Source/Adjacent Light/PawnRecentMemoryPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Adjacent_Light;
+
+[HarmonyPatch(typeof(PawnRecentMemory))]
+internal static class PawnRecentMemoryPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch("RecentMemoryInterval")]
+    private static void RecentMemoryIntervalPatch(Pawn ___pawn, ref int ___lastLightTick)
+    {
+        if (!___pawn.Spawned)
+        {
+            return;
+        }
+
+        if (___lastLightTick >= Find.TickManager.TicksGame)
+        {
+            return;
+        }
+
+        var room = ___pawn.GetRoom();
+        foreach (var cell in GenAdj.CellsAdjacent8Way(___pawn))
+        {
+            if (!room.ContainsCell(cell) ||
+                ___pawn.Map.glowGrid.PsychGlowAt(cell) == 0)
+            {
+                continue;
+            }
+
+            ___lastLightTick = Find.TickManager.TicksGame;
+            return;
+        }
+    }
+}
